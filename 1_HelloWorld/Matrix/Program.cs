43a5e3ac739340bb63621a4b9b84ae195c0fcb1e
oskolkovlad <oskolkovlad@yandex.ru>
2020-02-22 23:09:42 +0300

    Init and add HelloWorld Project

---
+++ 1_HelloWorld/Matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            String [] phrases = { "Wake up, Neo...", "The Matrix has you.", "Folow the white rabbit.", "Knock, knock, Neo." };
+
+            foreach (String phrase in phrases)
+            {
+                foreach (char c in phrase)
+                {
+                    Console.Write(c);
+                    Thread.Sleep(200);
+                }
+
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
