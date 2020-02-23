54df62ef38e25aa58c0faad97c450b768a88dae7
oskolkovlad <oskolkovlad@yandex.ru>
2020-02-23 19:37:55 +0300

    Cycles

---
+++ 1_HelloWorld/Cycles/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*** FOR ***//
+
+            List<int> list = new List<int>
+            {
+                1, 2, 3, 4, 5
+            };
+
+            // for (<инициализация>; <условие>; <порядок выполнения>)
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(i);
+            }
+
+            //for(,,){} - бесконечный цикл
+
+
+            // foreach (var <Элемент массива> in <Массив>) - для работы с коллекциями
+            // !!! - нельзя присваивать элементам коллекции какое-то значение
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine($"\nLength:\t{list.Count}");
+
+
+            //********************************************************************//
+
+
+            //*** WHILE ***//
+
+            // while (<Условие>)
+            while (list.Count > 5)
+            {
+                list.RemoveAt(list.Count - 1);
+            }
+
+            // while (true) - бесконечный цикл
+
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine($"\nLength:\t{list.Count}");
+
+
+            //********************************************************************//
+
+
+            //*** DO ... WHILE ***//
+
+            // do ... while (<Условие>)
+            do
+            {
+                list.RemoveAt(list.Count - 1);
+
+            } while (list.Count > 3);
+
+            // do ... while (true) - бесконечный цикл
+
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine($"\nLength:\t{list.Count}");
+
+
+            //********************************************************************//
+
+
+            /*
+             * Для управления циклом в языке C# используются два оператора: break и continue.
+             * Оператор break используется для прерывания выполнения цикла.
+             * Оператор continue используется для перехода к следующей итерации цикла.
+            */
+
+
+            //********************************************************************//
+
+
+            List<int> listTask = new List<int>();
+            const int N = 5;
+            int count = 0;
+
+            Console.WriteLine("\nВведите {0} целых числел в список.", N);
+            while (count < N)
+            {
+                if (int.TryParse(Console.ReadLine(), out int parseResult)) {
+                    listTask.Add(parseResult);
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine("Введенное не является целым числом или чмслом вовсе. Повторите попытку...");
+                }
+            }
+
+            int res = 0;
+            for (int i = 0; i < listTask.Count; i++)
+            {
+                res += listTask[i];
+            }
+            Console.WriteLine("\nSum = {0}", res);
+
+            res = 0;
+            count = listTask.Count - 1;
+            while (count >= 0)
+            {
+                res -= listTask[count];
+                count--;
+            }
+            Console.WriteLine("Dif = {0}", res);
+
+            res = 1;
+            count = listTask.Count - 1;
+            do
+            {
+                res *= listTask[count];
+                count--;
+            } while (count >= 0);
+            Console.WriteLine("Mult = {0}\n", res);
+
+            foreach (int i in listTask)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1_HelloWorld/Cycles/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Cycles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cycles")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("68aed0e4-b5c9-4276-8dcc-fb5962a18355")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
