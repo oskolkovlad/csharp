4aa9774c38556823b09d10eb0c43dfae986c1dfb
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-02 19:54:15 +0300

    Create UserController

---
+++ FitnessApplication/Fitness.BL/Controller/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Fitness.BL.Model;
+
+namespace Fitness.BL.Controller
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Конструктор получения данных пользователя.
+        /// </summary>
+        /// <returns> Пользователь. </returns>
+        public UserController()
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                if(formatter.Deserialize(fs) is User user)
+                {
+                    User = user;
+                }
+                
+                // TODO: Пользователь null
+            }
+        }
+
+        /// <summary>
+        /// Конструктор создания нового контроллера пользователя.
+        /// </summary>
+        /// <param name="user"> Пользователь. </param>
+        public UserController(string userName, string genderName, DateTime birthDay, double weight, double height)
+        {
+            // TODO: Проверка
+
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, birthDay, weight, height);
+        }
+
+        /// <summary>
+        /// Пользователь.
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Сохранение данных пользователя.
+        /// </summary>
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+            using(var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, User);
+            }
+        }
+    }
+}
--- FitnessApplication/Fitness.CMD/Program.cs
+++ FitnessApplication/Fitness.CMD/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-//using Fitness.BL;
+﻿using Fitness.BL.Controller;
+using System;
 
 namespace Fitness.CMD
 {
@@ -7,6 +7,29 @@ namespace Fitness.CMD
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Доброго времени суток! Вас приветствует FitnessApplication.");
+            
+            Console.WriteLine("Введите имя пользователя:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите пол:");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения:");
+            var birthDay = DateTime.Parse(Console.ReadLine()); // TODO: переписать в TryParse
+
+            Console.WriteLine("Введите вес:");
+            var weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите рост:");
+            var height = double.Parse(Console.ReadLine());
+
+
+            UserController userController = new UserController(name, gender, birthDay, weight, height);
+            userController.Save();
+
+
+            Console.ReadKey();
         }
     }
 }
