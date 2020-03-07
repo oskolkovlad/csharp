f850e216cec5b315de57c29888e672165fead0bb
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-07 18:22:31 +0300

    AaDS: Set and HashSet

---
+++ 6_DataStructures/Set/ListSet.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Set
+{
+    class ListSet<T> : IEnumerable
+    {
+        public ListSet()
+        {
+            set = new List<T>();
+        }
+
+        public ListSet(IEnumerable<T> set)
+        {
+            this.set = set.ToList<T>();
+        }
+
+
+        private List<T> set;
+        public int Count { get; private set; }
+        public bool IsEmpty => Count == 0;
+
+
+        public void Add(T data)
+        {
+            if (!set.Contains(data))
+            {
+                set.Add(data);
+                Count++;
+            } 
+            //else
+            //   throw new ArgumentException("Нельзя добавить повторяющийся элемент!");
+        }
+
+        public void Remove(T data)
+        {
+            //if (Count == 0)
+            //    throw new NullReferenceException("Множество пустое!");
+
+            if (set.Contains(data))
+            {
+                set.Remove(data);
+                Count--;
+            }
+            //else
+            //    throw new ArgumentException("Данного элемента нет в множестве, следовательно его невозможно удалить!");
+        }
+
+        public bool Contains(T data)
+        {
+            if (set.Contains(data))
+                return true;
+            else
+                return false;
+        }
+
+
+        public ListSet<T> Union(ListSet<T> setB)
+        {
+            ListSet<T> union = new ListSet<T>();
+
+            foreach (var s in set)
+            {
+                union.Add(s);
+            }
+
+            foreach (var s in setB.set)
+            {
+                union.Add(s);
+            }
+
+            return union;
+
+            //return new ListSet<T>(set.Union(setB.set));
+        }
+
+        public ListSet<T> Intersection(ListSet<T> setB)
+        {
+            var result = new ListSet<T>();
+
+            foreach (var s1 in set)
+            {
+                foreach (var s2 in setB.set)
+                {
+                    if (s1.Equals(s2))
+                    {
+                        result.Add(s1);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+
+            //return new ListSet<T>(set.Intersect(setB.set));
+        }
+
+        public ListSet<T> Difference(ListSet<T> setB)
+        {
+            var result = new ListSet<T>(set);
+
+            foreach (var s in setB.set)
+            {
+                result.Remove(s);
+            }
+
+            return result;
+
+            //return new ListSet<T>(set.Except(setB.set));
+        }
+
+        public ListSet<T> SymmetricDifference(ListSet<T> setB)
+        {
+            var result = Union(setB);
+            var intersept = Intersection(setB);
+
+            foreach (var s in intersept.set)
+            {
+                result.Remove(s);
+            }
+
+            return result;
+
+            //return new ListSet<T>(set.Union(setB.set).Except(set.Intersect(setB.set)));
+        }
+
+        public bool SubSet(ListSet<T> setB)
+        {
+            var count = 0;
+
+            foreach (var s1 in set)
+            {
+                foreach (var s2 in setB.set)
+                {
+                    if (s1.Equals(s2))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            if (count == setB.Count)
+                return true;
+            else
+                return false;
+
+            //return setB.set.All(s => set.Contains(s));
+        }
+
+        public override string ToString()
+        {
+            return $"В множестве следующее количество элементов: {Count}";
+        }
+
+
+        /*public IEnumerator GetEnumerator()
+        {
+            foreach(var s in set)
+            {
+                yield return s;
+            }
+        }*/
+
+        public IEnumerator GetEnumerator() => /*((IEnumerable)this).GetEnumerator();*/ set.GetEnumerator();
+    }
+}
---
+++ 6_DataStructures/Set/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - HashSet<T> содержит неповторяющюся коллекцию элементов.
+            // - HashSet<T> содержит элементы неупорядоченно (без сортировки). 
+            // - HashSet<T> позволяет быстро определить, есть такой элемент или нет (быстро потому что,
+            // использует индекс, который вычисляется из хэш-кода элемента). 
+            // - HashSet < T > -имеет методы Add, Remove, Contains, но поскольку он использует хэш-реализацию,
+            // эти операции занимают 1 действие (методы Contains и Remove в List < T > занимает n - действий.)
+            
+            // HashSet<T> имеет методы:
+            //  - UnionWith(объединение элементов с другим HashSet<T>)
+            //  - IntersectWith(пересечение элементов с другим HashSet<T>)
+            //  - ExceptWith(разность элементов с другим HashSet<T>)
+            //  - SymmetricExceptWith (симетрическая разность элементов с другим HashSet<T>)
+
+
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+            set1.Add(1);
+            set1.Add(4);
+            set1.Add(5);
+            set1.Add(3);
+            set1.Add(777);
+            //set1.Add(777);
+
+            set2.Add(1);
+            set2.Add(45);
+            set2.Add(23);
+            set2.Add(3);
+            set2.Add(777);
+
+            Console.Write("set1: ");
+            foreach(var s in set1)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("set2: ");
+            foreach (var s in set2)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            
+            
+
+
+            set1.UnionWith(set2);
+            Console.Write("Union: ");
+            foreach (var s in set1)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            set1 = new HashSet<int>() { 1, 4, 5, 3, 777};
+            set1.IntersectWith(set2);
+            Console.Write("Intersect: ");
+            foreach (var s in set1)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            set1 = new HashSet<int>() { 1, 4, 5, 3, 777 };
+            set1.ExceptWith(set2);
+            Console.Write("Except 1 - 2: ");
+            foreach (var s in set1)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            set1 = new HashSet<int>() { 1, 4, 5, 3, 777 };
+            set2.ExceptWith(set1);
+            Console.Write("Except 2 - 1: ");
+            foreach (var s in set2)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            set1.SymmetricExceptWith(set2);
+            set2 = new HashSet<int>() { 1, 45, 23, 3, 777 };
+            Console.Write("Sym Dif: ");
+            foreach (var s in set1)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine(new string('=', 50));
+            Console.WriteLine();
+
+
+            //****************************************************************************//
+
+
+            // Каждый элемент множества (Set) уникален.
+            // Они могут быть неупорядоченными.
+
+            // - Объединения (Union).
+            // - Пересечение (intersection).
+            // - Разность - (difference).
+            // - Симметричная разность - (symmetric difference).
+            // - Подмножество - (subset).
+
+            ListSet<int> listSet = new ListSet<int>();
+            ListSet<int> listSet1 = new ListSet<int>();
+            ListSet<int> listSet2 = new ListSet<int>();
+
+            listSet.Add(1);
+            listSet.Add(4);
+            listSet.Add(5);
+            listSet.Add(3);
+            listSet.Add(777);
+            //listSet.Add(777);
+
+            listSet1.Add(1);
+            listSet1.Add(45);
+            listSet1.Add(23);
+            listSet1.Add(3);
+            listSet1.Add(777);
+
+            listSet2.Add(3);
+            listSet2.Add(777);
+
+            Console.WriteLine(listSet);
+            Console.WriteLine(listSet.Contains(777));
+            Console.WriteLine(listSet.Contains(777777));
+            Console.WriteLine();
+
+            foreach(var s in listSet)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+
+            listSet.Remove(5);
+            listSet.Remove(1);
+            //listSet.Remove(45);
+            Console.WriteLine();
+
+            Console.WriteLine(listSet);
+
+            Console.Write("listSet: ");
+            foreach (var s in listSet)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("listSet1: ");
+            foreach (var s in listSet1)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("listSet2: ");
+            foreach (var s in listSet2)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine(new string('+', 50));
+            Console.WriteLine();
+
+            var diff = listSet.Difference(listSet1);
+            var union = listSet.Union(listSet1);
+            var intersect = listSet.Intersection(listSet1);
+            var sub = listSet1.SubSet(listSet2);
+            var sDiff = listSet.SymmetricDifference(listSet1);
+            Console.WriteLine();
+
+
+            Console.Write("Union: ");
+            foreach (var s in union)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Intersection: ");
+            foreach (var s in intersect)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference: ");
+            foreach (var s in diff)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Symmetric Difference: ");
+            foreach (var s in sDiff)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("SubSet: {0}", sub);
+            Console.WriteLine();
+
+            Console.Write("listSet: ");
+            foreach (var s in listSet)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("listSet1: ");
+            foreach (var s in listSet1)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+
+
+            //****************************************************************************//
+
+
+            Console.ReadKey();
+        }
+    }
+}
