1e9d59f891fb99371b4f03dc1616f4c503ad242f
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-10 18:11:33 +0300

    AaDS: Binary Heap

---
+++ 6_DataStructures/BinaryHeap/Heap.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace BinaryHeap
+{
+    class Heap<T> : IComparable<T>, IEnumerable<T>
+        where T : IComparable<T>
+    {
+        private List<T> heap;
+
+        public Heap()
+        {
+            heap = new List<T>();
+        }
+
+        public Heap(List<T> items)
+        {
+            heap = new List<T>();
+            heap.AddRange(items);
+            
+            for (var i = 0; i < Count; i++)
+            {
+                Sort(i);
+            }
+        }
+
+
+        public T Data { get; private set; }
+        public int Count => heap.Count;
+        public bool IsEmpty => Count == 0;
+
+
+        public T PeekMaxNode()
+        {
+            if (!IsEmpty)
+            {
+                return heap[0];
+            }
+
+            return default;
+        }
+
+        public void Add(T data)
+        {
+            heap.Add(data);
+
+            var currentIndex = Count - 1;
+            var parentIndex = GetParentIndex(currentIndex);
+
+            while (currentIndex > 0 && heap[parentIndex].CompareTo(heap[currentIndex]) == -1)
+            {
+                Swap(currentIndex, parentIndex);
+
+                currentIndex = parentIndex;
+                parentIndex = GetParentIndex(currentIndex);
+            }
+        }
+
+        private int GetParentIndex(int currentIndex) => (currentIndex - 1) / 2;
+
+        private void Swap(int currentIndex, int parentIndex)
+        {
+            var item = heap[currentIndex];
+            heap[currentIndex] = heap[parentIndex];
+            heap[parentIndex] = item;
+        }
+
+
+        public T PopMaxNode()
+        {
+            if (!IsEmpty)
+            {
+                var result = heap[0];
+                heap[0] = heap[Count - 1];
+                heap.RemoveAt(Count - 1);
+                Sort(0);
+
+                return result;
+            }
+
+            return default;
+        }
+
+        private void Sort(int currentIndex)
+        {
+            int maxIndex;
+            int leftIndex;
+            int rightIndex;
+
+            while (currentIndex < Count)
+            {
+                IndexesForRootLeftRight(currentIndex, out maxIndex, out leftIndex, out rightIndex);
+
+                if (leftIndex < Count && heap[leftIndex].CompareTo(heap[maxIndex]) == 1)
+                {
+                    maxIndex = leftIndex;
+                }
+
+                if (rightIndex < Count && heap[rightIndex].CompareTo(heap[maxIndex]) == 1)
+                {
+                    maxIndex = rightIndex;
+                }
+
+                if (maxIndex == currentIndex)
+                {
+                    break;
+                }
+
+                Swap(currentIndex, maxIndex);
+                currentIndex = maxIndex;
+            }
+        }
+
+        private void IndexesForRootLeftRight(int currentIndex, out int rootIndex, out int leftIndex, out int rightIndex)
+        {
+            rootIndex  = currentIndex;
+            leftIndex  = currentIndex * 2 + 1;
+            rightIndex = currentIndex * 2 + 2;
+        }
+
+        public int CompareTo([AllowNull] T other) => this.CompareTo(other);
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = new List<T>(heap);
+
+            while (!IsEmpty)
+            {
+                yield return PopMaxNode();
+            }
+
+            heap = current;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+    }
+}
---
+++ 6_DataStructures/BinaryHeap/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BinaryHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Stopwatch();
+            var rnd = new Random();
+            var items = new List<int>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                items.Add(rnd.Next(-100, 1000));
+            }
+
+            timer.Start();
+            Heap<int> heap = new Heap<int>(items);
+            timer.Stop();
+            Console.WriteLine("timer = " + timer.Elapsed);
+
+            timer.Restart();
+            for (var i = 0; i < 100; i++)
+            {
+                heap.Add(rnd.Next(-100, 1000));
+            }
+            timer.Stop();
+            Console.WriteLine("timer = " + timer.Elapsed);
+
+            Console.WriteLine();
+            timer.Restart();
+            foreach (var h in heap)
+            {
+                Console.WriteLine(h);
+            }
+            timer.Stop();
+            Console.WriteLine();
+            Console.WriteLine("timer = " + timer.Elapsed);
+
+            //Console.WriteLine(heap.PeekMaxNode());
+            //Console.WriteLine(heap.PopMaxNode());
+
+
+            Console.ReadKey();
+        }
+    }
+}
