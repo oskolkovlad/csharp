8b7c5bf8d0eb373bf9bc878a408c0717efef2123
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-13 22:15:14 +0300

    LINQ: Aggregate Operations

--- 8_LINQ/Sets_Aggregate_SkipTake_GroupBy/Program.cs
+++ 8_LINQ/Sets_Aggregate_SkipTake_GroupBy/Program.cs
@@ -55,29 +55,59 @@ namespace Sets_Aggregate_SkipTake_GroupBy
             {
                 Console.WriteLine(s);
             }
+            Console.WriteLine(new string('=', 50));
+
+
+            //*****************************************************************************//
+
+            int[] numbers = { 1, 2, 3, 4, 10, 34, 55, 66, 77, 88 };
+
+            // Метод Aggregate
+            int mult = numbers.Aggregate((x, y) => x + y);
+            Console.WriteLine(mult);
             Console.WriteLine();
 
 
+            // Получение размера выборки. Метод Count
+            int size = (from i in numbers where i % 2 == 0 && i > 10 select i).Count();
+            Console.WriteLine(size);
+
+            int _size = numbers.Count(i => i % 2 == 0 && i > 10);
+            Console.WriteLine(_size);
+            Console.WriteLine();
+
+
+            // Получение суммы
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
+            Console.WriteLine();
+
+
+            // Максимальное, минимальное и среднее значения
+            int min = numbers.Min();
+            int max = numbers.Max();
+            double avr = numbers.Average();
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+            Console.WriteLine(avr);
+
+
+            Console.WriteLine(new string('=', 50));
+
+
+
             //*****************************************************************************//
 
 
 
 
-
-
-
-            //*****************************************************************************//
-
-
-
-
-
+            Console.WriteLine(new string('=', 50));
 
             //*****************************************************************************//
 
 
 
-
+            Console.WriteLine(new string('=', 50));
 
 
 
