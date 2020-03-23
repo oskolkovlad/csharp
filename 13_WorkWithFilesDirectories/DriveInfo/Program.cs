bcefa7b98f846e31ebe0ddde4d6b49a841f7774f
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-23 21:45:08 +0300

    Work With Files and Directories (WWFaD): DriveInfo.

---
+++ 13_WorkWithFilesDirectories/DriveInfo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _DriveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var drive in DriveInfo.GetDrives())
+            {
+                Console.WriteLine("Название диска: " + drive.Name);
+                Console.WriteLine($"Метка диска: {drive.VolumeLabel ?? "Метка отсутствует"}");
+                Console.WriteLine("Состояние готовности: " + drive.IsReady);
+                Console.WriteLine("Общий объем диска: " + (drive.TotalSize / Math.Pow(1024, 3)).ToString("f2"));
+                Console.WriteLine("Объем свободного пространства: " + (drive.TotalFreeSpace / Math.Pow(1024, 3)).ToString("f2"));
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
