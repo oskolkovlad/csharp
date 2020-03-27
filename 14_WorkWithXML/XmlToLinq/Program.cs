c0e9af96507c4ac1d5762725813931a16cddba1e
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-27 21:30:06 +0300

    XML: Create XML file with LINQ to XML.

---
+++ 14_WorkWithXML/XmlToLinq/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace XmlToLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*XDocument xdoc = new XDocument();
+            // создаем первый элемент
+            XElement iphone6 = new XElement("phone");
+            // создаем атрибут
+            XAttribute iphoneNameAttr = new XAttribute("name", "iPhone 6");
+            XElement iphoneCompanyElem = new XElement("company", "Apple");
+            XElement iphonePriceElem = new XElement("price", "40000");
+            // добавляем атрибут и элементы в первый элемент
+            iphone6.Add(iphoneNameAttr);
+            iphone6.Add(iphoneCompanyElem);
+            iphone6.Add(iphonePriceElem);
+
+            // создаем второй элемент
+            XElement galaxys5 = new XElement("phone");
+            XAttribute galaxysNameAttr = new XAttribute("name", "Samsung Galaxy S5");
+            XElement galaxysCompanyElem = new XElement("company", "Samsung");
+            XElement galaxysPriceElem = new XElement("price", "33000");
+            galaxys5.Add(galaxysNameAttr);
+            galaxys5.Add(galaxysCompanyElem);
+            galaxys5.Add(galaxysPriceElem);
+            // создаем корневой элемент
+            XElement phones = new XElement("phones");
+            // добавляем в корневой элемент
+            phones.Add(iphone6);
+            phones.Add(galaxys5);
+            // добавляем корневой элемент в документ
+            xdoc.Add(phones);
+            //сохраняем документ
+            xdoc.Save("phones.xml");*/
+
+            XDocument xdoc = new XDocument(new XElement("phones",
+                new XElement("phone",
+                    new XAttribute("name", "iPhone 6"),
+                    new XElement("company", "Apple"),
+                    new XElement("price", "40000")),
+                new XElement("phone",
+                    new XAttribute("name", "Galaxy S5"),
+                    new XElement("company", "Samsung"),
+                    new XElement("price", "33000"))
+                ));
+
+            xdoc.Save("phones.xml");
+
+
+            //************************************************************************//
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
