096ffaf98a168ee42f4f02d6c4ba46fa5484fa08
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-29 15:51:33 +0300

    Application domain

---
+++ 15_Processes_Validation/ApplicationDomain/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace ApplicationDomain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // При запуске приложения, написанного на C#, операционная система создает процесс,
+            // а среда CLR создает внутри этого процесса логический контейнер, который называется
+            // доменом приложения и внутри которого работает запущенное приложение.
+
+            // Для управления домена платформа .NET предоставляет класс AppDomain.
+
+            AppDomain appDomain = AppDomain.CurrentDomain;
+            Console.WriteLine(appDomain.Id);
+            Console.WriteLine(appDomain.FriendlyName);
+            Console.WriteLine(appDomain.BaseDirectory);
+            Console.WriteLine();
+
+            foreach (Assembly assem in appDomain.GetAssemblies())
+            {
+                Console.WriteLine(assem.GetName().Name);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
